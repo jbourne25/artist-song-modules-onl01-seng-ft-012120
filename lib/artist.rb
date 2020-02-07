1726cf6073ad100afed880f28fac9c5a16c3acc2
Jeremy Bourne <jbourne25@yahoo.com>
2020-02-07 02:04:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,7 @@ class Artist
   
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  include Memorable::InstanceMethods
+  #include Memorable::InstanceMethods
   include Paramble::InstanceMethods
   
   
